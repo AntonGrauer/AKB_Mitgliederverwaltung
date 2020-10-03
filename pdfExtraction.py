849a76d8b75d4a2a9de511a7b48ed05ef8a1563e
AntonGrauer <61539231+AntonGrauer@users.noreply.github.com>
2020-10-03 23:58:15 +0200

    Working on list with all information included in Antrag

--- pdfExtraction.py
+++ pdfExtraction.py
@@ -1,18 +1,35 @@
 import pdftotext
 
-# Load your PDF
+# Load your PDF and save all text to a txt file
 with open("Mitgliedsdaten.pdf", "rb") as f:
     antragPDF = pdftotext.PDF(f)
 
-# Save all text to a txt file.
 with open('Mitgliedsdaten.txt', 'w') as f:
     f.write("\n\n".join(antragPDF))
 
 antragCleared = open("Mitgliedsdaten.txt", "r")
 
-for line in antragCleared:
-    lineFirstName = line.find('Vorname')
-    if lineFirstName != -1:
-        lineFirstNameCleared = line[lineFirstName+len('Vorname')+1:]
-        firstName = ''.join(lineFirstNameCleared.split())
-        print (firstName)
+# Creating method in order to streamline subsequent assignments
+def getValue (valueName, antrag):
+    firstName = 0
+    for line in antrag:
+        lineFirstName = line.find(valueName)
+        if lineFirstName != -1:
+            lineFirstNameCleared = line[lineFirstName+len(valueName)+1:]
+            firstName = ''.join(lineFirstNameCleared.split())
+    return firstName
+
+vorname = getValue("Vorname", antragCleared)
+print (vorname)
+
+categories = ("Vorname", "Nachname", "Telefon (mobil)", "E-Mail Adresse", "Geburtstag (Datum)",
+              "Straße", "PLZ", "Stadt", "Telefon (priv.)", "IBAN", "BIC", "Studiengang", "Eintrittssemester")
+
+allInformation = list()
+
+for var in categories:
+        allInformation.append(getValue(var, antragCleared))
+
+print (allInformation)
+
+
