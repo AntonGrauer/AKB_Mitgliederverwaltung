ae7f6563b4474378f3e86bf55596bc46603f27bb
AntonGrauer <61539231+AntonGrauer@users.noreply.github.com>
2020-10-09 19:43:06 +0200

    GSuite-Anpassung

--- pdfExtraction.py
+++ pdfExtraction.py
@@ -1,6 +1,10 @@
-import pdftotext
 import csv
 import os
+import random
+import string
+import pandas
+import pdftotext
+
 
 # Creating method in order to streamline subsequent assignments
 def getValue (dataname, valueName):
@@ -22,27 +26,54 @@ def getInformation (fileName):
         allInformation.append(getValue(fileName, value))
     return allInformation
 
-# Creating method that writes the whole data into a csv-file
+# creating method to create email address
+def createMail( prename, surname):
+    maillist = pandas.read_excel("Mitgliederliste_Test.xlsx")
+    mail = surname + "@akboerse.de"
+    prename.replace(" ","")
+    a = 0
+    while mail in maillist: #to prevent addresses from being handed to more than one person
+        mail = mail[:a]+ prename[a] + mail[a:]
+        a += 1
+    return mail
+
+#method to generate passkeys
+def createPasskey():
+    letters_and_digits = string.ascii_letters + string.digits
+    return ''.join((random.choice(letters_and_digits) for i in range(8)))
+
+# Creating method that writes data into every database
 def sendInformation(data):
     with open('Vorlage_BVH.csv', 'w') as csvfile:
         writer = csv.writer(csvfile, delimiter=';')
         writer.writerows(data)
+    gsuitedata = []
+    for i in range(0, len(data)):  # data gets formated so it only contains necessary information
+        gsuitedata.append(
+            [data[i][0], data[i][1], createMail(data[i][0], data[i][1]), createPasskey(), "", "/Normale Mitglieder", "",
+             data[i][3]])
+    with open('Vorlage_GSuite.csv', 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=';')
+        writer.writerows(gsuitedata)
+
 
 # Umsetzung der Methoden
-antraegeDirectory = "./Mitgliedsanträge"
-allFiles = os.listdir(antraegeDirectory)
-allFiles.pop(0)
-finalInformation = []
-for antraege in allFiles:
-    with open("./Mitgliedsanträge/" + antraege, "rb") as f:
-        antragPDF = pdftotext.PDF(f)
+def start():
+    antraegeDirectory = "./Mitgliedsanträge"
+    allFiles = os.listdir(antraegeDirectory)
+    allFiles.pop(0)
+    finalInformation = []
+    for antraege in allFiles:
+        with open("./Mitgliedsanträge/" + antraege, "rb") as f:
+            antragPDF = pdftotext.PDF(f)
 
-    with open("./Mitgliedsanträge/" + antraege + '.txt', 'w') as f:
-        f.write("\n\n".join(antragPDF))
+        with open("./Mitgliedsanträge/" + antraege + '.txt', 'w') as f:
+            f.write("\n\n".join(antragPDF))
 
-    antragInformation = getInformation(antraege + '.txt')
-    print (antragInformation)
-    finalInformation.append(antragInformation)
-    os.remove("./Mitgliedsanträge/" + antraege + '.txt')
+        antragInformation = getInformation(antraege + '.txt')
+        print (antragInformation)
+        finalInformation.append(antragInformation)
+        os.remove("./Mitgliedsanträge/" + antraege + '.txt')
 
-sendInformation(finalInformation)
+    sendInformation(finalInformation)
+start()
